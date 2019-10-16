a8d4931c61dc8b922b9c9e54f1b98ecd7ecd0d79
1001bug <alex2000@1001bug.ru>
2019-10-17 00:29:38 +0300

    add

--- yaml_cfg_tree_pub.h
+++ yaml_cfg_tree_pub.h
@@ -24,7 +24,10 @@ extern "C" {
     typedef void * Yvoid_t;
     typedef void ** PYvoid_t;
     
-    Yvoid_t YreadCfg(char* file);
+    void print_t(Yvoid_t *Tree);
+    void destroy_t(Yvoid_t *Tree);
+    
+    Yvoid_t YreadCfg(FILE *fd, char* file);
     
     //зачем везде PYvoid_t????
     

13ca8fad2e14d8ef3a23003994bea3448f550691
1001bug <alex2000@1001bug.ru>
2019-10-17 00:37:33 +0300

    Netbeans project files

---
+++ build/Debug/GNU-Linux/main.o.d
@@ -0,0 +1,3 @@
+build/Debug/GNU-Linux/main.o: main.c yaml_cfg_tree_pub.h
+
+yaml_cfg_tree_pub.h:
---
+++ build/Debug/GNU-Linux/yaml_cfg_tree.o.d
@@ -0,0 +1,6 @@
+build/Debug/GNU-Linux/yaml_cfg_tree.o: yaml_cfg_tree.c yaml_cfg_tree.h \
+ yaml_cfg_tree_pub.h
+
+yaml_cfg_tree.h:
+
+yaml_cfg_tree_pub.h:
